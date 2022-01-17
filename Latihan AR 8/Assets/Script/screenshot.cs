a9f5443ee253037c53d4230dc4d7bf64d666a4cf
Helmika Priyanto <helmika.zero@gmail.com>
2022-01-17 15:55:21 +0700

    change init

---
+++ Latihan AR 8/Assets/Script/screenshot.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class screenshot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator Screenshot()
+    {
+        yield return new WaitForEndOfFrame();
+        Texture2D texture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+
+        texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        texture.Apply();
+
+        string name = "Screenshot_EpicApp" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
+        
+        
+        //MOBILE
+        NativeGallery.SaveImageToGallery(texture, "myapp_pictures", name);
+
+        Destroy(texture);
+    }
+
+    public void TakeScreenshot()
+    {
+        StartCoroutine("Screenshot");
+    }
+}
