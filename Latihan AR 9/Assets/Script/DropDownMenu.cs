286da186855cf2b97a47e692d24d0eadbab008ac
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2022-03-07 15:32:23 +0700

    jos

--- Latihan AR 9/Assets/Script/DropDownMenu.cs
+++ Latihan AR 9/Assets/Script/DropDownMenu.cs
@@ -7,6 +7,12 @@ public class DropDownMenu : MonoBehaviour
 {
     public float fadeSpeed = 1f;
 
+    public RectTransform dropMenu;
+    public RectTransform dropButton;
+    public RectTransform dropButtonFollowPos;
+
+    public bool isOpen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +22,21 @@ public class DropDownMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        dropButton.position = dropButtonFollowPos.position;
+    }
+
+
+    public void TOGGLE_DROP()
+    {
+        isOpen = !isOpen;
+
+        if (isOpen)
+        {
+            dropMenu.DOAnchorPosY(0f, fadeSpeed, false);
+        }
+        else
+        {
+            dropMenu.DOAnchorPosY(dropMenu.rect.height, fadeSpeed, false);
+        }
     }
 }
