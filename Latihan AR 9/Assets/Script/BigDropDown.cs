0a3dca53ddf7913b1bc431a0b2592da5dfea96e6
Helmika Priyanto <68269642+helmikazero@users.noreply.github.com>
2022-03-23 13:48:48 +0700

    big dropdown

--- Latihan AR 9/Assets/Script/BigDropDown.cs
+++ Latihan AR 9/Assets/Script/BigDropDown.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class DropDownMenu : MonoBehaviour
+public class BigDropDown : MonoBehaviour
 {
     public float fadeSpeed = 1f;
 
