b406fff846d7b21414c5d91dc045b9b45ba94a1b
DiditDitapuri <97897969+DiditDitapuri@users.noreply.github.com>
2022-05-13 11:47:44 +0700

    Didit Edit Controller Audio

--- Latihan AR 9/Assets/Script/TutorialSequenceManager.cs
+++ Latihan AR 9/Assets/Script/TutorialSequenceManager.cs
@@ -20,6 +20,9 @@ public class TutorialSequenceManager : MonoBehaviour
 
     public int currentSequenceIndex;
 
+    public Transform TutorialPopUp;
+    public Sprite ImageAudioOn;
+    public Sprite ImageAudioOff;
 
     // Start is called before the first frame update
     void Start()
@@ -40,19 +43,25 @@ public class TutorialSequenceManager : MonoBehaviour
 
     public void NEXT_PAGE()
     {
+        All_ToggleController();
+
         for (int i = 0; i < tutorialSequence[currentSequenceIndex].itemsToSow.Length; i++)
         {
             tutorialSequence[currentSequenceIndex].itemsToSow[i].SetActive(false);
-            tutorialSequence[currentSequenceIndex].voiceRecorder.Stop();
+            
         }
+
+        tutorialSequence[currentSequenceIndex].voiceRecorder.Stop();
 
         currentSequenceIndex++;
 
         for (int i = 0; i < tutorialSequence[currentSequenceIndex].itemsToSow.Length; i++)
         {
             tutorialSequence[currentSequenceIndex].itemsToSow[i].SetActive(true);
-            tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
+            
         }
+
+        tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
 
         if (tutorialSequence[currentSequenceIndex].isOn)
         {
@@ -62,23 +71,30 @@ public class TutorialSequenceManager : MonoBehaviour
         {
             tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
         }
+
+        
     }
 
     public void PREV_PAGE()
     {
+        All_ToggleController();
+
         for (int i = 0; i < tutorialSequence[currentSequenceIndex].itemsToSow.Length; i++)
         {
-            tutorialSequence[currentSequenceIndex].itemsToSow[i].SetActive(false);
-            tutorialSequence[currentSequenceIndex].voiceRecorder.Stop();
+            tutorialSequence[currentSequenceIndex].itemsToSow[i].SetActive(false);      
         }
+
+        tutorialSequence[currentSequenceIndex].voiceRecorder.Stop();
 
         currentSequenceIndex--;
 
         for (int i = 0; i < tutorialSequence[currentSequenceIndex].itemsToSow.Length; i++)
         {
             tutorialSequence[currentSequenceIndex].itemsToSow[i].SetActive(true);
-            tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
+            
         }
+
+        tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
 
         if (tutorialSequence[currentSequenceIndex].isOn)
         {
@@ -88,6 +104,10 @@ public class TutorialSequenceManager : MonoBehaviour
         {
             tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
         }
+
+
+        
+
     }
 
     public void END_TUTORIAL ()
@@ -95,21 +115,26 @@ public class TutorialSequenceManager : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void MuteToggle (Toggle mute)
+    public void MuteToggle ()
     {
-        if (mute.isOn)
+
+        if (tutorialSequence[currentSequenceIndex].isOn)
         {
-            tutorialSequence[currentSequenceIndex].voiceRecorder.Stop();
-            MuteAudio();
+            tutorialSequence[currentSequenceIndex].isOn = false;
+            tutorialSequence[currentSequenceIndex].voiceRecorder.Play();         
+            All_AudioController();
+            All_ToggleController();
         }
         else
         {
-            tutorialSequence[currentSequenceIndex].voiceRecorder.Play();
-            MuteAudio();
+            tutorialSequence[currentSequenceIndex].isOn = true;
+            tutorialSequence[currentSequenceIndex].voiceRecorder.Stop();            
+            All_AudioController();
+            All_ToggleController();
         }
     }
 
-    public void MuteAudio ()
+  /*  public void MuteAudio ()
     {
         if (tutorialSequence[currentSequenceIndex].isOn)
         {
@@ -119,7 +144,38 @@ public class TutorialSequenceManager : MonoBehaviour
         {
             tutorialSequence[currentSequenceIndex].isOn = true;
         }
-    }
+    }*/
     
+    public void All_AudioController ()
+    {
+        for (int i = 0; i < tutorialSequence.Length; i++)
+        {
+            if (tutorialSequence[currentSequenceIndex].isOn)
+            {
+                tutorialSequence[i].isOn = true;
+            }
+            else
+            {
+                tutorialSequence[i].isOn = false;
+            }
+        }
+    }
 
+    public void All_ToggleController ()
+    {
+        for (int i = 0; i < TutorialPopUp.childCount; i++)
+        {
+            if (tutorialSequence[currentSequenceIndex].isOn)
+            {
+                TutorialPopUp.GetChild(i).GetChild(3).GetComponent<Image>().sprite = ImageAudioOff;
+            }
+            else
+            {
+                TutorialPopUp.GetChild(i).GetChild(3).GetComponent<Image>().sprite = ImageAudioOn;
+            }
+        }
+
+
+           
+    }
 }
